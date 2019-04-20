8f6bc67420dbc6a9f7704eb47084ae269f9efe5a
Rosh <rosh_408@hotmail.com>
2019-04-19 20:08:13 -0700

    finishing touches on newsletter section

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+//All of your code goes under
+console.log('this is working');
+
+var flkty = new Flickity( '.main-carousel', {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    freeScroll: true
+  });
+
+}); //end of doc ready
