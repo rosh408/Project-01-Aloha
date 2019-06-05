dfc63a00e8dfbea30e58094c0a0004a33ebf2b8d
Rosh <rosh_408@hotmail.com>
2019-06-05 11:07:17 -0700

    Validate email in email subscription section

--- js/main.js
+++ js/main.js
@@ -1,27 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){
-
-var flkty = new Flickity( '.main-carousel', {
-    // options
-    cellAlign: 'left',
+document.addEventListener("DOMContentLoaded", function() {
+  let flkty = new Flickity(".main-carousel", {
+    cellAlign: "left",
     contain: true,
     freeScroll: true
   });
-document.getElementById("subscribe-form").addEventListener('submit', function(e){
-e.preventDefault();
-const emailInsert = document.getElementsByClassName('email-container');
-function validateEmail(email) {
-  return re.test(String(email).toLowerCase());
-}
-console.log(emailInsert.value());
-  if (emailInsert.value() == validateEmail() ) {
-    alert("Thank you for subscribing!");
-  } else {
-    alert("Please insert valid email address!");
-  }
-});
 
-}); //end of doc ready
-
-
-
-
+  document
+    .getElementById("subscribe-form")
+    .addEventListener("submit", function(e) {
+      e.preventDefault();
+      const emailInsert = document.getElementById("email-container");
+      const emailInput = emailInsert.value;
+      function validateEmail() {
+        return emailInput.toLowerCase();
+      }
+      if (
+        emailInput === validateEmail() &&
+        emailInput.includes("@") &&
+        emailInput.includes(".com")
+      ) {
+        emailInsert.value = "";
+        setTimeout(function() {
+          alert("Thank you for subscribing!");
+        }, 250);
+      } else {
+        alert("Please insert valid email address!");
+      }
+    });
+    
+}); 
